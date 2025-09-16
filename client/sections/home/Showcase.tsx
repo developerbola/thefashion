5de654c31c098e1c53a46a54aabb4491ef3f41ab
developerbola <developerbola08@gmail.com>
2025-09-16 15:46:00 +0500

    show case finish

--- client/sections/home/Showcase.tsx
+++ client/sections/home/Showcase.tsx
@@ -1,18 +1,21 @@
 const Showcase = () => {
   return (
-    <div className="relative h-[calc(100vh-64px)] w-full flex justify-end bg-[#d4d4d4]">
-      <div className="absolute left-[5%] top-[5%]">
+    <div className="relative h-[50vw] md:h-[calc(100vh-64px)] w-full flex justify-end bg-[#156ef7] overflow-clip">
+      <div className="absolute left-[3%] top-[5%] z-2">
         <p
-          className="uppercase text-[170px] font-medium whitespace-nowrap"
-          style={{ lineHeight: 0.9 }}
+          className="uppercase font-medium whitespace-nowrap text-white"
+          style={{
+            fontSize: "clamp(2.5rem, 10vw, 12.5rem)",
+            lineHeight: 0.9,
+          }}
         >
           Build <br /> in style
         </p>
       </div>
       <img
-        src="/showcase.avif"
+        src="/showcase.webp"
         alt="showcase image"
-        className="h-full w-auto"
+        className="h-full w-auto scale-150 md:scale-200 mt-[80px] md:mt-[400px] md:mr-[180px]"
       />
     </div>
   );
